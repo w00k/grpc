3dec31978814d6a3e3adf576d0d6300416805ebe
Francisco Arenas <franciscoarenas.globant@latam.com>
2023-02-14 16:52:28 -0300

    add fix for docker-compose and improve in postgres for get student

--- database/postgres.go
+++ database/postgres.go
@@ -22,28 +22,32 @@ func NewPostgresRepository(url string) (*PostgresRepository, error) {
 }
 
 func (repo *PostgresRepository) SetStudent(ctx context.Context, student *models.Student) error {
+	log.Println("values: ", student.Id, " - ", student.Name, " - ", student.Age)
 	_, err := repo.db.ExecContext(ctx, "INSERT INTO students (id, name, age) VALUES ($1, $2, $3)", student.Id, student.Name, student.Age)
 	return err
 }
 
 func (repo *PostgresRepository) GetStudent(ctx context.Context, id string) (*models.Student, error) {
+	log.Println("value: ", id)
 	rows, err := repo.db.QueryContext(ctx, "SELECT id, name, age FROM students WHERE id = $1", id)
 	if err != nil {
 		return nil, err
 	}
-	defer func() {
-		err := rows.Close()
-		log.Fatal(err)
-	}()
 
 	var student = models.Student{}
 	for rows.Next() {
-		if err := rows.Scan(&student.Id, &student.Name, &student.Age); err == nil {
+		if err = rows.Scan(&student.Id, &student.Name, &student.Age); err == nil {
 			return &student, nil
 		}
 	}
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
+
+	defer func() {
+		err := rows.Close()
+		log.Fatal(err)
+	}()
+
 	return &student, nil
 }
--- server-student/main.go
+++ server-student/main.go
@@ -12,23 +12,26 @@ import (
 )
 
 func main() {
-	list, err := net.Listen("tcp", "5060")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	repo, err := database.NewPostgresRepository("postgres://postgres:postgres@localhost:54321/postgres?sslmode=disable")
+	list, err := net.Listen("tcp", ":5060")
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error listening: %s", err.Error())
 	}
+
+	repo, err := database.NewPostgresRepository("postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
 
 	server := server.NewServer(repo)
+
+	if err != nil {
+		log.Fatalf("Error creating repository: %s", err.Error())
+	}
+
 	s := grpc.NewServer()
 	studentpb.RegisterStudentServiceServer(s, server)
+
 	reflection.Register(s)
 
 	if err := s.Serve(list); err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error serving: %s", err.Error())
 	}
 }
